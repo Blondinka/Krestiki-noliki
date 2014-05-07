1711bea6cbec3244df4e77b52b411607fcb21fa5
dj Sharomet <sokolowladik@mail.ru>
2014-05-07 18:36:18 +0300

    second commit

--- KNClient.java
+++ KNClient.java
@@ -8,21 +8,47 @@ public class KNClient {
 	public static void main(String[] args){
 		Scanner scanner=new Scanner(System.in);
 		Socket socket=null;
+		int x,y;
+		boolean game=true;
 		try{
-			socket=new Socket(InetAddress.getLocalHost(),8033);
-			PrintStream ps=new PrintStream(socket.getOutputStream());
-			BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String msg=br.readLine();
-			System.out.println('>'+msg);
-			System.out.print('<');
-			msg=scanner.nextLine();
-			ps.println(msg);
-			ps.flush();
-			ps.close();
-			socket.close();	
+			Pole pole=new Pole();
+			socket=new Socket(InetAddress.getLocalHost(),8031);
+			while(game==true){
+				PrintStream ps=new PrintStream(socket.getOutputStream());
+				BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String s=new String();
+				while(pole.isKrestikiWin()==false && pole.isNolikikiWin()==false){
+					System.out.println("Wait while your opponent is moving");
+					s=br.readLine();
+					x=Integer.valueOf(s);
+					s=br.readLine();
+					y=Integer.valueOf(s);
+					pole.setKrestik(x, y);
+					pole.display();
+					if(pole.isKrestikiWin()==false){
+						pole.move();
+						x=pole.getLastX();
+						y=pole.getLastY();
+						pole.display();
+						s=String.valueOf(x);
+						ps.println(s);
+						s=String.valueOf(y);
+						ps.println(s);
+						ps.flush();
+					}
+				}
+				if(pole.isKrestikiWin()==true)
+					System.out.println("You are loose");
+				if(pole.isNolikikiWin()==true)
+					System.out.println("You are win");
+				if(pole.isFull()==true)
+					System.out.println("Draw!");
+				ps.close();
+			}
+			socket.close();
 		}
 		catch(IOException e){
-				e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
 }
--- KNServer.java
+++ KNServer.java
@@ -1,33 +1,66 @@
 package ServerPack;
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
 public class KNServer {
 	public static void main(String[] args)throws IOException{
-		Socket socket=null;
-		try{
-			Scanner scanner=new Scanner(System.in);
-			ServerSocket server=new ServerSocket(8033);
-			System.out.println("server is started");
-			Pole pole=new Pole();
-			pole.move();
-			pole.display();
-			socket=server.accept();
-			PrintStream ps=new PrintStream(socket.getOutputStream());
-			BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String msg="привет";
-			ps.println(msg);
-			ps.flush();
-			msg=br.readLine();
-			System.out.println(msg);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		finally{
-			if(socket!=null)
-				socket.close();
-		}	
+		boolean game=true;
+		Scanner scan=new Scanner(System.in);
+			Socket socket=null;
+			int x,y;
+			try{
+				ServerSocket server=new ServerSocket(8031);
+				Pole pole=new Pole();
+				socket=server.accept();
+				while(game==true){
+					System.out.println("The game is started");
+					PrintStream ps=new PrintStream(socket.getOutputStream());
+					BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					String s=new String();
+					while(pole.isKrestikiWin()==false && pole.isNolikikiWin()==false && pole.isFull()==false){
+						pole.move();
+						x=pole.getLastX();
+						y=pole.getLastY();
+						pole.display();
+						s=String.valueOf(x);
+						ps.println(s);
+						s=String.valueOf(y);
+						ps.println(s);
+						ps.flush();
+						if(pole.isKrestikiWin()==false){
+							System.out.println("Wait while your opponent is moving");
+							s=br.readLine();
+							x=Integer.valueOf(s);
+							s=br.readLine();
+							y=Integer.valueOf(s);
+							pole.setNolik(x, y);
+							pole.display();
+						}
+					}
+					if(pole.isKrestikiWin()==true)
+						System.out.println("You are win");
+					if(pole.isNolikikiWin()==true)
+						System.out.println("You are loose");
+					if(pole.isFull()==true)
+						System.out.println("Draw!");
+					System.out.println("Do you want try again?");
+					System.out.println("Yes-any integer");
+					System.out.println("No-0");
+					int choise=scan.nextInt();
+					ps.close();
+					if(choise==0)
+						game=false;
+				}
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+			finally{
+				if(socket!=null)
+					socket.close();
+			}
 	}
 }
+
